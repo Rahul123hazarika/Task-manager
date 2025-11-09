7787347e6d57fd5f670f3e4479d452c0b2839e9b
RAHUL HAZARIKA <98834787+Rahul123hazarika@users.noreply.github.com>
2025-11-09 21:26:49 +0530

    Create DashboardPage.jsx

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard/Dashboard';
+
+const DashboardPage = () => {
+  return (
+    <div className="container max-w-7xl mx-auto px-4 py-8">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
